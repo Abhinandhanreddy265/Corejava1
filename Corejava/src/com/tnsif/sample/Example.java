88100367aafff671566961143884f8c886f7ab7f
shash <shash@shashi>
2025-02-10 21:45:34 +0530

    first

--- Corejava/src/com/tnsif/sample/Example.java
+++ Corejava/src/com/tnsif/sample/Example.java
@@ -1,7 +1,7 @@
 package com.tnsif.sample;
 
 public class Example {
-	public static void main(String[] args) {
-		System.out.println("hello");
-	}
+public static void main(String[] args) {
+	System.out.println("hello");
+}
 }
