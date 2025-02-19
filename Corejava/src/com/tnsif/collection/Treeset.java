1e22dd8dcc6efaadc0b8ddb34d336f7b695214f1
shash <shash@shashi>
2025-02-19 21:13:42 +0530

    six

---
+++ Corejava/src/com/tnsif/collection/Treeset.java
@@ -0,0 +1,19 @@
+package com.tnsif.collection;
+import java.util.TreeSet;
+public class Treeset {
+public static void treesetoperation() {
+	TreeSet<Integer>t=new TreeSet<Integer>();
+	t.add(78);
+	t.add(8);
+	t.add(58);
+	t.add(3);
+	t.add(3);
+	System.out.println("elements of treeset"+t);}
+	public static void main(String[] args) {
+		Treeset.treesetoperation();
+	}
+	{
+
+	}
+}
+
---
+++ Corejava/src/com/tnsif/set/stack.java
@@ -0,0 +1,25 @@
+package com.tnsif.set;
+import java.util.Stack;
+
+public class stack {
+private static final Stack<Integer> ob = null;
+public static void main(String[]args) {
+	Stack<Integer>ob=new Stack<Integer>();
+	ob.push(1);
+	ob.push(7);
+	ob.push(5);
+	ob.push(3);
+System.out.println("Stack elements"+ob);
+System.out.println(ob.peek());
+if(ob.isEmpty());
+System.out.println("Stack is empty");
+}
+ {
+System.out.println("not empty");
+{
+System.out.println(ob.search(7));
+}
+}
+}
+
+
